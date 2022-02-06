bbbff8571780e9467874b09d5830f0d0de752fef
Romanno <Romanno@users.noreply.github.com>
2022-02-06 14:22:14 +0000

    deploy: 7561af7f6aead3777302d5359df09ae54befadcf

---
+++ precache-manifest.0d451553cf3d3cd9ab5f5a2567cb3146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30ee609837a10e4bb3efc5a1ec79773",
+    "url": "https://Romanno.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "27f61a3b9c8c4836b1b1",
+    "url": "https://Romanno.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "994711f7fdd1df5bfb23",
+    "url": "https://Romanno.github.io/task-management-frontend/static/js/2.12cee1ba.chunk.js"
+  },
+  {
+    "revision": "27f61a3b9c8c4836b1b1",
+    "url": "https://Romanno.github.io/task-management-frontend/static/js/main.71d91ba4.chunk.js"
+  },
+  {
+    "revision": "0f66236117ca37134dd3",
+    "url": "https://Romanno.github.io/task-management-frontend/static/js/runtime~main.a4e6a042.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://Romanno.github.io/task-management-frontend/precache-manifest.0d451553cf3d3cd9ab5f5a2567cb3146.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://Romanno.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
